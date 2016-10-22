ef2b481cc739138b1d347434b1980191154efbbf
AgustinPrado <agusprado92@gmail.com>
2016-10-21 23:17:39 -0300

    20161021 - Clase 16 Test Unitario
    Empecé el ejercicio pero no lo seguí.

---
+++ prog2_2daParte/20161021_Clase16_TestUnitarioLibrary/NacionalidadInvalidaException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20161021_Clase16_TestUnitarioLibrary
+{
+    public class NacionalidadInvalidaException : Exception
+    {
+        public NacionalidadInvalidaException()
+            : base("La nacionalidad no se condice con el número de DNI")
+        {
+        }
+        public NacionalidadInvalidaException(string message)
+            : base("La nacionalidad no se condice con el número de DNI: " + message)
+        {
+        }
+    }
+}
---
+++ prog2_2daParte/20161021_TestUnitario/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _20161021_Clase16_TestUnitarioLibrary;
+
+namespace _20161021_TestUnitario
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Persona per = new Persona("Agus", "Prado", 0, Persona.ENacionalidad.Argentino);
+
+        }
+    }
+}
